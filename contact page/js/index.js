f2b0990420805244ae7bf379b92479bf7bd85b2b
tara <asma_toobaie@yaoo.com>
2020-09-16 21:55:18 -0400

    more iteration

---
+++ contact page/js/index.js
@@ -0,0 +1,29 @@
+
+console.log("helloWorld");
+
+
+
+var myObj = { firsname : "tara", lastname : "toobaie" };
+console.log(myObj);
+
+$(".topnav a").mouseover(function(){
+    $(this).css("color", "#f2994a");
+    $(this).css("text-effects", "drop shadow");
+
+    
+
+
+  });
+  $(".topnav a").mouseout(function(){
+    $(this).css("color", "black");
+
+  });
+
+
+
+
+
+$("input submit").hover(function(){
+    $(this).css("background-color", "#red");
+
+  });
---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+console.log("helloWorld");
+
+
+
+var myObj = { firsname : "tara", lastname : "toobaie" };
+console.log(myObj);
+
+$(".navbar a").mouseover(function(){
+    $(this).css("color", "#f2994a");
+    $(this).css("text-effects", "drop shadow");
+
+    
+
+
+  });
+  $(".navbar a").mouseout(function(){
+    $(this).css("color", "black");
+
+  });
+
+
+
+  $(".footer-icon img").mousover(function(){
+    $(this).style("width", "30px");
+
+  });
+
+  $(".footer-icon img").mouseout(function(){
+    $(this).style("width", "20px");
+
+  });
+
+
+
+  
+  
+
+
